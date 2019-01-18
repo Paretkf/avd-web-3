75dd536c6c099bb098a70f70822f1da696b7eafe
Paretkf <paretk7@gmail.com>
2019-01-18 15:51:58 +0700

    adv-web-day-3

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Rachata Tongpakdee</h1>
+    <h2>IT-4RC</h2>
+  </div>
+</template>
---
+++ src/views/Cal.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Calculate</h1>
+    <input type="radio" value="rec" v-model="shape">รูป 4 เหลี่ยม<br>
+    <input type="radio" value="circle" v-model="shape">รูปวงกลม<br>
+    <div v-if="shape === 'circle'">
+      รัศมี: <input type="text" v-model="circle.radius"> หน่วย<br><br>
+      พื้นที่: {{(22/7) * (circle.radius * circle.radius)}} หน่วย
+    </div>
+    <div v-else>
+      กว้าง: <input type="text" v-model="rectangle.width"> หน่วย<br>
+      สูง(ยาว): <input type="text" v-model="rectangle.height"> หน่วย<br><br>
+      พื้นที่: {{ rectangle.width * rectangle.height}} หน่วย
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      shape: 'rec',
+      circle: {
+        radius: 0
+      },
+      rectangle: {
+        width: 0,
+        height: 0
+      }
+    }
+  }
+}
+</script>

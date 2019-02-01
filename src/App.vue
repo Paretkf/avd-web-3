f4126bbb3b510f84865760604e45a24eb0e8827c
Paretkf <paretk7@gmail.com>
2019-02-01 14:33:58 +0700

    add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <Nav/>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import Nav from '@/components/nav.vue'
+export default {
+  components: {
+    Nav
+  }
 }
+</script>
+
+<style>
+
 </style>
---
+++ src/components/nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-cl-danger cl-white dp-flex pd-20px f-s-24px f-w-bold">
+   <div class="cs-pointer mg-hrzt-10px">
+     Home
+   </div>
+   <div class="cs-pointer">
+     About
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>

25829452507125978a0ebccf1c27642ac3736b60
Paretkf <paretk7@gmail.com>
2019-02-01 13:57:53 +0700

    remove file

--- src/router.js
+++ src/router.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Fluk from './views/Fluk.vue'
-import Cal from './views/Cal.vue'
-import Grade from './views/Grade.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -13,26 +10,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    },
-    {
-      path: '/about-me',
-      name: 'fluk',
-      component: Fluk
-    },
-    {
-      path: '/cal',
-      name: 'cal',
-      component: Cal
-    },
-    {
-      path: '/grade',
-      name: 'grade',
-      component: Grade
     }
   ]
 })
--- src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Rachata Tongpakdee</h1>
-    <h2>IT-4RC</h2>
-  </div>
-</template>

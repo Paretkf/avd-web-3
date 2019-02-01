1f188191d455cd9c0c5febf1ed1f5eb9c67ce0f4
Paretkf <paretk7@gmail.com>
2019-02-01 15:08:47 +0700

    get data

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="bg-cl-danger cl-white dp-flex pd-20px f-s-24px f-w-bold">
-   <div class="cs-pointer mg-hrzt-10px">
-     Home
-   </div>
-   <div class="cs-pointer">
-     About
-   </div>
+  <div class="bg-cl-light cl-white dp-flex pd-20px f-s-24px f-w-bold">
+    <div class="cs-pointer mg-hrzt-10px">
+      <router-link :to="{ name: 'home'}">
+        Home
+      </router-link>
+    </div>
+    <div class="cs-pointer mg-hrzt-10px">
+      <router-link :to="{ name: 'about'}">
+        About
+      </router-link>
+    </div>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="home">
-    <h1>Home Page</h1>
+  <div class="home t-al-center">
+    <h1 class="f-s-36px f-w-bold">Home Page</h1>
+    {{results}}
   </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      results: []
+    }
+  },
+  async mounted () {
+    this.results = await axios.get('https://api.giphy.com/v1/gifs/search?api_key=2jDWxGFa3ks3pUPg2ZbkMlLYRRWkddz1&q=kitty&limit=25&offset=0&rating=G&lang=en')
+    console.log('results', this.results)
+  }
 }
 </script>
